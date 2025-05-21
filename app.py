669431a6bfd2e3383f906b556de251a3f0aa91d7
Dovydas T <trijonisdovydas1995@gmail.com>
2025-05-21 23:51:25 +0300

    Refactor user authentication and registration system
    - Introduced a new UserProfile model to store additional user details.
    - Updated the AuthUser model to establish a relationship with UserProfile.
    - Replaced password hashing with Flask-Bcrypt for improved security.
    - Enhanced the registration process to create a user profile upon registration.
    - Modified login functionality to utilize Flask-Login for session management.
    - Updated forms to reflect changes in user registration and profile management.
    - Redesigned base HTML structure for better navigation and user experience.
    - Improved CSS styles for a modern look and feel, including responsive design.
    - Removed unused validators and cleaned up code for better maintainability.
    - Created migration scripts to reflect database schema changes.

--- app.py
+++ app.py
@@ -5,9 +5,9 @@ from flask import Flask, flash, render_template, redirect, request, url_for, ses
 from flask_sqlalchemy import SQLAlchemy
 from config import database_url
 from flask_migrate import Migrate
-from forms import LoginForm, RegisterForm
-from werkzeug.security import generate_password_hash, check_password_hash
-
+from forms import LoginForm, RegisterForm, RegisterDetailsForm
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+from flask_bcrypt import Bcrypt
 
 
 app = Flask(__name__)
@@ -16,24 +16,32 @@ app.config['SQLALCHEMY_DATABASE_URI'] = database_url()
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+login_manager = LoginManager(app)
+login_manager.login_view = 'index'
+bcrypt = Bcrypt(app)
 
 
-
-
-class User(db.Model):
+# Database Models
+class AuthUser(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
     password_hash = db.Column(db.String(255), nullable=False)
-    name = db.Column(db.String(30), nullable=False)
-    email = db.Column(db.String(35), unique=True, nullable=False)
-    birth_date = db.Column(db.Date, nullable=False)
-    vin = db.Column(db.String(17), unique=True, nullable=False)
 
+    profile = db.relationship('UserProfile', backref='auth_user', uselist=False)
     posts = db.relationship('Post', backref='creator', lazy=True)
 
     def __repr__(self):
-        return f"User('{self.id}', '{self.username}')"
-    
+        return f"AuthUser('{self.id}', '{self.username}')"
+
+class UserProfile(db.Model):
+    id = db.Column(db.Integer, db.ForeignKey('auth_user.id'), primary_key=True)
+    name = db.Column(db.String(30), nullable=True)
+    email = db.Column(db.String(35), unique=True, nullable=True)
+    birth_date = db.Column(db.Date, nullable=True)
+    created_at = db.Column(db.DateTime, nullable=False, default=db.func.current_timestamp())
+
+    def __repr__(self):
+        return f"UserProfile('{self.id}', '{self.created_at}')"
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -41,24 +49,14 @@ class Post(db.Model):
     content = db.Column(db.Text, nullable=False)
     created_at = db.Column(db.DateTime, nullable=False, default=db.func.current_timestamp())
     
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('auth_user.id'), nullable=False)
 
     def __repr__(self):
         return f"Post('{self.title}', User ID: {self.user_id})"
 
-
-
-def login_required(f):
-    @wraps(f)
-    def decodated_function(*args, **kwargs):
-        if 'user_id' not in session:
-            flash('You need to log in first.', 'error')
-            return redirect(url_for('login'))
-        return f(*args, **kwargs)
-    return decodated_function
-
-
-
+@login_manager.user_loader
+def load_user(user_id):
+    return AuthUser.query.get(int(user_id))
 
 
 @app.route('/')
@@ -72,93 +70,63 @@ def register():
     if form.validate_on_submit():
         username = form.username.data
         password = form.password.data
-        name = form.name.data
-        email = form.email.data
-        birth_date = form.birth_date.data
-        vin = form.vin.data
-        
-        hashed_password = generate_password_hash(password)
+        user = AuthUser.query.filter_by(username=username).first()
 
-        new_user = User(
-            username=username,
-            password_hash=hashed_password,
-            name=name,
-            email=email,
-            birth_date=birth_date,
-            vin=vin
-        )
+        if user:
+            flash("Username already exists", "error")
+            return render_template('register.html', form=form)
         
+        hashed_password = bcrypt.generate_password_hash(password).decode('utf-8')
+        new_user = AuthUser(username=username, password_hash=hashed_password)
+        user_profile = UserProfile(name=None, email=None, birth_date=None)
+
+        #FIXME: Maybe needs to be changed to user_profile = UserProfile()
+        new_user.profile = user_profile
+        #user_profile = UserProfile(id=new_user.id, name=None, email=None, birth_date=None)
+        #db.session.add(user_profile)
+        #db.session.commit()
+
         db.session.add(new_user)
         db.session.commit()
-        
         flash(f"Account created for {new_user.username}!", "success")
-        return redirect(url_for('login'))  # Change to a success page if needed
+        return redirect(url_for('login'))
     
     return render_template("register.html", form=form)
 
-
+    
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
-    if form.validate_on_submit():
+    if request.method == 'POST':
         username = form.username.data
         password = form.password.data
-        query = db.select(User).filter_by(username=username)
-        user_result = db.session.execute(query).first()
-        if user_result is None:
-            flash("User not found", "error")
-            return render_template('login.html', form = form)
-        user : User = user_result[0]
-        if check_password_hash(user.password_hash, password):
-            session.permanent = True
-            session["user_id"] = user.id
-            session["username"] = user.username
+        user = AuthUser.query.filter_by(username=username).first()
 
-            flash("User is logged in", "success")
+        if user and bcrypt.check_password_hash(user.password_hash, password):
+            flash(f"Welcome back, {user.username}!", "success")
+            login_user(user)
             return redirect(url_for('index'))
-        
-
-        flash("Incorect password", "error")
-        return render_template('login.html', form = form)
-    
-    return render_template('login.html', form = form)
-
-@app.route('/posts')
-def all_posts():
-    if posts in Post.query.order_by(Post.created_at.desc()).all():
-        if 'user_id' in session:
-                user_posts = user.posts
-                if not user_posts:
-                    flash("This user has no posts", "info")
-    else:
-        flash("No posts available", "info")
+        else:
+            flash("Invalid username or password", "error")
+            return render_template('login.html', form=form)
+    return render_template('login.html', form=form)
 
 
-    posts = Post.query.order_by(Post.created_at.desc()).all()
-    if not posts:
-        flash("No posts available", "info")
-
-    user = User.query.get(session["user_id"])
-    if user:        
-        user_posts = user.posts
-        if not user_posts:
-            flash("This user has no posts", "info")
-    if 'user_id' in session:
-        user = User.query.get(session["user_id"])
-        flash("User not logged in", "warning")
-    return render_template('posts.html', posts=posts, user_posts=user_posts)
-
-
-@app.route('/system')
+@app.route('/profile')
 @login_required
-def system():
-    return render_template('system.html')
-
+def profile():
+    user = AuthUser.query.get(current_user.id)
+    if user:
+        return render_template('profile.html', user=user)
+    else:
+        flash("User not logged in", "error")
+        return redirect(url_for('index'))
 
 @app.route('/logout')
+@login_required
 def logout():
-    session.clear()
-    flash("User is logged out", "success")
+    logout_user()
+    flash("User logged out", "success")
     return redirect(url_for('index'))
 
 
--- forms.py
+++ forms.py
@@ -1,20 +1,17 @@
 from flask_wtf import FlaskForm
 from wtforms import DateField, StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired, Length
-from validators import validate_vin, validate_unique_username, validate_unique_email
 
 class RegisterForm(FlaskForm):
-    username = StringField('Username',validators=[DataRequired(), Length(min=3, max=20), validate_unique_username])
+    username = StringField('Username',validators=[DataRequired(), Length(min=3, max=20)])
     password = PasswordField('Password',validators=[DataRequired(), Length(min=6)])
-    name = StringField('Name',validators=[DataRequired(), Length(min=2, max=30)])
-    email =StringField('Email',validators=[DataRequired(), Length(min=6, max=35), validate_unique_email])
-    birth_date = DateField('Birth Date (YYYY-MM-DD)', format='%Y-%m-%d', validators=[DataRequired()])
-    vin = StringField('VIN', validators=[DataRequired(), Length(min=17, max=17), validate_vin])
     submit =SubmitField('Register')
 
-    def __init__(self, model=None, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self._model = model
+class RegisterDetailsForm(FlaskForm):
+    name = StringField('Name', validators=[DataRequired(), Length(min=2, max=30)])
+    email = StringField('Email', validators=[DataRequired(), Length(min=6, max=35)])
+    birth_date = DateField('Birth Date (YYYY-MM-DD)', format='%Y-%m-%d', validators=[DataRequired()])
+    submit = SubmitField('Register Details')
 
 
 class LoginForm(FlaskForm):
